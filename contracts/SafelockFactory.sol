98427a2fd5e30e8276e35ae267003e3a6df04869
Boboye-Ak <boboye771@gmail.com>
2022-10-07 21:11:02 +0100

    wrote view functions for SafelockFactory.sol

--- contracts/SafelockFactory.sol
+++ contracts/SafelockFactory.sol
@@ -17,13 +17,29 @@ contract SafelockFactory {
         s_latestSafelockId = 0;
     }
 
+    //Pure Functions
     function createSafelock() public {
-        if(s_hasSafelock[msg.sender]!=0){
+        if (s_hasSafelock[msg.sender] != 0) {
             revert SafelockFactory__AlreadyHasSafelock();
         }
         s_latestSafelockId += 1;
         Safelock newSafelock = new Safelock(s_latestSafelockId, msg.sender, address(this));
         s_hasSafelock[msg.sender] = s_latestSafelockId;
         s_safelocks.push(newSafelock);
+    }
+
+    //View Functions
+    function getMySafelockAddress() public view returns (address) {
+        uint256 id = s_hasSafelock[msg.sender];
+        if (id == 0) {
+            return address(0);
+        } else {
+            return address(s_safelocks[id - 1]);
+        }
+    }
+
+    function getMySafelockId() public view returns (uint256) {
+        uint256 id = s_hasSafelock[msg.sender];
+        return id;
     }
 }
