e46fab968c6d343186f347d3a3bfdaebc3c0a775
Boboye-Ak <boboye771@gmail.com>
2022-10-07 19:02:05 +0100

    Wrote CreateSafelock function in SafelockFactory.sol

--- contracts/SafelockFactory.sol
+++ contracts/SafelockFactory.sol
@@ -8,9 +8,22 @@ contract SafelockFactory {
     //State Variables
     mapping(address => uint256) s_hasSafelock;
     uint256 s_latestSafelockId;
-    Safelock[] s_safelocks; 
+    Safelock[] s_safelocks;
+
+    //Custom Errors
+    error SafelockFactory__AlreadyHasSafelock();
 
     constructor() {
         s_latestSafelockId = 0;
+    }
+
+    function createSafelock() public {
+        if(s_hasSafelock[msg.sender]!=0){
+            revert SafelockFactory__AlreadyHasSafelock();
+        }
+        s_latestSafelockId += 1;
+        Safelock newSafelock = new Safelock(s_latestSafelockId, msg.sender, address(this));
+        s_hasSafelock[msg.sender] = s_latestSafelockId;
+        s_safelocks.push(newSafelock);
     }
 }
