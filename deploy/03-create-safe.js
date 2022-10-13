ddabb06e392f7744df1974cbc74309532697f66c
Boboye-Ak <boboye771@gmail.com>
2022-10-13 09:17:24 +0100

    Wrote deploy scripts to create two safelocks

---
+++ deploy/03-create-safe.js
@@ -0,0 +1,16 @@
+const { ethers, network } = require("hardhat")
+const { networkConfig } = require("../helper-hardhat-config")
+const { getSigners } = ethers
+
+module.exports = async ({ deployments, getNamedAccounts }) => {
+    const user = (await getSigners())[1]
+    const { log } = deployments
+    const safelockFactory = await ethers.getContract("SafelockFactory", user)
+    const mySafelockAddress = await safelockFactory.getMySafelockAddress()
+    const safelock = await ethers.getContractAt("Safelock", mySafelockAddress, user)
+    log("-------creating 2 safelocks-------")
+    await safelock.createSafe("172800", { value: ethers.utils.parseEther("2") })
+    await safelock.createSafe("172800", { value: ethers.utils.parseEther("2") })
+}
+
+module.exports.tags = ["all", "create-safe"]
